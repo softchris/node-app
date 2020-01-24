79c04d0e5000928e26bca18be8a9f3d3568e55cc
softchris <noringc@gmail.com>
2020-01-24 23:52:58 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+const products = [
+  {
+    id: 1,
+    name: "Star Wars"
+  }
+];
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on ${port} port!`))
